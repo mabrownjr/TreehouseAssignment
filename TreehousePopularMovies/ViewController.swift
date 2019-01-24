21c2831da04ac045c5e15cd2b455d03afda38a62
Michael Brown <mbrown@aaas.org>
2019-01-24 13:45:48 -0500

    Having issues with getting the "results" array. Not sure why I'm error catching out.

--- TreehousePopularMovies/ViewController.swift
+++ TreehousePopularMovies/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    final let url = URL(string: "https://api.themoviedb.org/3/movie/550?api_key=c3df1ec1d614f1f10ffb71cd1cb8a112")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        fetchJSON()
+    }
+    
+    func fetchJSON (){
+        
+        guard let downloadURL = url else { return }
+        URLSession.shared.dataTask(with: downloadURL) { (data, urlResponse, error) in
+            guard let data = data, error == nil, urlResponse != nil else {
+                print("something is wrong")
+                return
+            }
+            print("Downloaded")
+            do {
+                let decoder = JSONDecoder()
+                let results = try decoder.decode([Results].self, from: data)
+                print(results)
+            }catch {
+                print("Something wrong after downloading")
+            }
+            
+            
+            
+            
+            
+        }.resume()
     }
 
 
